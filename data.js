7315d9c0d4d293dcd84d1128d9d7f3059fd9dde7
Seyi Ogundipe <ogemgoldsmith07@gmail.com>
2023-11-05 15:02:17 +0100

    Fourth commit

--- data.js
+++ data.js
@@ -11,14 +11,14 @@ const data = [
       category: "Fashion",
       link: "#",
       copy: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta perferendis assumenda cupiditate placeat quidem cumque vel iusto delectus eaque minus.",
-      imgSrc: "hero2.jpg",
+      imgSrc: "https://img.freepik.com/free-photo/blurred-shiny-textured-silver-soft-feather_23-2148114528.jpg?w=740&t=st=1699192733~exp=1699193333~hmac=acd1a4fea22a7eb4172bfaebc78bc52e61cc1d9ff779a952ee11ef4c614db61c",
     },
     {
       name: "Atmospheric Aero-adaption",
       category: "Technology",
       link: "#",
       copy: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta perferendis assumenda cupiditate placeat quidem cumque vel iusto delectus eaque minus.",
-      imgSrc: "hero3.jpg",
+      imgSrc: "https://images.unsplash.com/photo-1533907650686-70576141c030?auto=format&fit=crop&q=80&w=1470&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "3D Holographic Printing",
